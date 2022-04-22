24e6151081abbd84048dcce105db3b1465afb088
Shelby Rothman <shelby.rothman@aderant.com>
2022-04-22 14:03:16 -0700

    set auth outside of app

--- API/wit.js
+++ API/wit.js
@@ -1,20 +1,19 @@
 const axios = require('axios');
 
-const sendMessage = async (message) => {
+
+
+const sendMessage = async (message, token) => {
     try {
         const response = await axios.get(`https://api.wit.ai/message?v=20220422&q=${message}`, {
             headers: {
-                Authorization: 'Bearer 5WOWFMQQCBBKZPAUXN2ZHCOZMPU7XLUL'
+                Authorization: `Bearer ${token}`
             }
         });
-        // console.log(response.data);
         return response.data;
     } catch (error) {
         throw new Error(error);
     }
 }
 
-
-// console.log(sendMessage().then(res => console.log(res)).catch(err => console.log(err)));
 
 module.exports = sendMessage;
--- server.js
+++ server.js
@@ -1,27 +1,22 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express');
 const path = require('path');
 const app = express();
-const config = require('./config');
-
-// const WitService = require('./API/wit');
 app.use(express.urlencoded({ extended: true }))
-// const router = require('./API/router');
-
 const sendMessage = require('./API/wit');
 
-// app.use('/bots/slack', slackRouter({ reservationService, witService, config }));
-// const witService = new WitService(config.wit.token);
-
-
+const token = process.env.WIT_TOKEN;
 
 app.get('/send', async (req, res) => {
     console.log(req.query.message);
     let message = req.query.message.toString();
     message = message.replace(/\s/g, '%20');
-    await sendMessage(message)
+    await sendMessage(message, token)
         .then(resp => res.send(resp))
         .catch(err => console.error(err));
-    // res.send(response);
 });
 
 app.get('/', (req, res) => {
