165e1196a3421e9d84ad93a24c41d8dd62d86d09
Shelby Rothman <shelby.rothman@aderant.com>
2022-04-25 14:26:16 -0700

    handleMention method now working as expected. Next begin building sessionService

--- JS/main.js
+++ JS/main.js
@@ -11,16 +11,16 @@ const witService = async (mem) => {
         data: {
             message: mem
         },
-        success: function (data) {
-            console.log(data)
+        success: async (data) => {
+
             if (data.intents[0].confidence > 0.8) {
-                // appendMessage(`Bot: ${JSON.stringify(data, null, 2)}`)
-                appendMessage(`Bot: ${handleMention(data)}`)
+                const botRes = await handleMention(data);
+                appendMessage(`Bot: ${botRes}`)
             } else {
                 appendMessage(`Bot: I'm sorry, can you rephrase that?`)
             }
         },
-        error: function (data) {
+        error: (data) => {
             console.log(data)
         }
     })
@@ -45,21 +45,25 @@ const appendMessage = (message) => {
     messageContainer.append(messageElement)
 }
 
-const handleMention = (data) => {
+const handleMention = async (data) => {
     let text = 'Hey!';
     const entities = data.entities;
+    const intents = data.intents;
 
-    const { intent, customerName, reservationDateTime, numberOfGuests } = entities;
+    const customerName = entities['wit$contact:contact'][0].value;
+    const reservationDateTime = entities['wit$datetime:reservationDateTime'][0].value;
+    const numberOfGuests = entities['wit$number:numberOfGuests'][0].value;
 
-    if (!intent || intent !== 'reservation' || !customerName || !reservationDateTime || !numberOfGuests) {
-        text = 'Sorry - could you rephrase that and include details?';
-        console.log(entities);
-    } else {
-        text = 'The table was successfully reserved!'
-        //TODO: rework in here the reservationService to check and return the right text and perform necessary edits to db
+
+    for (let i = 0; i < intents.length; i++) {
+        if (intents[i].name !== 'reservation' || !customerName || !reservationDateTime || !numberOfGuests) {
+            text = 'Sorry - could you rephrase that and include details?';
+            console.log(entities) //debug
+        } else {
+            text = 'The table was successfully reserved!'
+            //TODO: rework in here the reservationService to check and return the right text and perform necessary edits to db(i.e. tables full or not)
+        }
     }
-
     return text;
-
 }
 
